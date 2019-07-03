1cb81c9d7533d43437c09d0dc69398e57d7a7717
larry852 <larryportocarrero@gmail.com>
2019-07-02 23:26:24 -0500

    def skip profile on lists

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ def likes():
         'token') is None else request.args.get('token')
     url = 'https://api.gotinder.com/v2/fast-match/teasers'
     data = get_data(token, url)
+    remove_profile(data, '81bb473a-d11c-46f1-8825-a4b49c88ba34')
     return render_template('likes.html', data=data.get('data').get('results'), token=token)
 
 
@@ -40,6 +41,16 @@ def get_data(token, url):
     request = urllib.request.Request(url, headers=headers)
     response = urllib.request.urlopen(request).read()
     data = json.loads(response.decode("utf-8"))
+    return data
+
+
+def remove_profile(data, id):
+    try:
+        index = [item['user']['_id']
+                 for item in data['data']['results']].index(id)
+        del data['data']['results'][index]
+    except:
+        pass
     return data
 
 
