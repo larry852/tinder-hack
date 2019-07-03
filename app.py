be7d23446dc76254c60f7b6b3f692ca57dc1ab58
larry852 <larryportocarrero@gmail.com>
2019-07-02 21:03:39 -0500

    Add search funcion

--- app.py
+++ app.py
@@ -6,16 +6,35 @@ app = Flask(__name__)
 TOKEN = '3e8cd831-519e-433c-9941-3fdc1a71868d'
 
 
-@app.route('/', methods=['GET'])
-def index():
+@app.route('/likes', methods=['GET'])
+def likes():
     token = TOKEN if request.args.get(
         'token') is None else request.args.get('token')
-    data = get_data(token)
-    return render_template('index.html', data=data.get('data').get('results'))
-
-
-def get_data(token):
     url = 'https://api.gotinder.com/v2/fast-match/teasers'
+    data = get_data(token, url)
+    return render_template('likes.html', data=data.get('data').get('results'))
+
+
+@app.route('/search', methods=['GET'])
+def search():
+    token = TOKEN if request.args.get(
+        'token') is None else request.args.get('token')
+    url = 'https://api.gotinder.com/v2/recs/core'
+    data = get_data(token, url)
+    return render_template('search.html', data=data.get('data').get('results'))
+
+
+@app.route('/match', methods=['GET'])
+def match():
+    token = TOKEN if request.args.get(
+        'token') is None else request.args.get('token')
+    id = request.args.get('id')
+    url = 'https://api.gotinder.com/like/{}'.format(id)
+    get_data(token, url)
+    return "NEW MATCH!!!"
+
+
+def get_data(token, url):
     headers = {}
     headers['X-Auth-Token'] = token
     request = urllib.request.Request(url, headers=headers)
