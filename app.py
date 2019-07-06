13e98bc7115f3879b780c9dafca3640220bb78dd
larry852 <larryportocarrero@gmail.com>
2019-07-06 17:06:31 -0500

    Fix bug token like

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ def likes():
         'token') is None else request.args.get('token')
     url = 'https://api.gotinder.com/v2/fast-match/teasers'
     data = get_data(token, url)
-    remove_profile(data, '81bb473a-d11c-46f1-8825-a4b49c88ba34')
     return render_template('likes.html', data=data.get('data').get('results'), token=token)
 
 
@@ -44,7 +43,7 @@ def get_data(token, url):
     return data
 
 
-def remove_profile(data, id):
+def exclude_profile_list(data, id):
     try:
         index = [item['user']['_id']
                  for item in data['data']['results']].index(id)
